fdfc114778b5d3f1dc78a8dd894bb16c91e1853c
Liu Han <hanliu061433@gmail.com>
2018-06-29 14:22:52 -0700

    Encapsulate logic for handling requests into a request_handler module

---
+++ chalicelib/request_handler.py
@@ -0,0 +1,52 @@
+import hashlib
+import json
+import boto3
+
+from enum import Enum
+from botocore.exceptions import ClientError
+
+BUCKET_NAME = "hca-dcp-matrix-service-request-status"
+
+
+class RequestStatus(Enum):
+    UNINITIALIZED = "UNINITIALIZED"
+    RUNNING = "RUNNING"
+    DONE = "DONE"
+
+
+class RequestHandler:
+    @staticmethod
+    def generate_request_id(bundle_uuids):
+        """
+        Generate a request id based on a list of bundle uuids
+        :param bundle_uuids: A list of bundle uuids
+        :return: Request id
+        """
+        bundle_uuids.sort()
+        m = hashlib.sha256()
+
+        for bundle_uuid in bundle_uuids:
+            m.update(bundle_uuid.encode())
+
+        return m.hexdigest()
+
+    @staticmethod
+    def check_request_status(request_id):
+        """
+        Check the status of a matrix concatenation request
+        :param request_id: Matrices concatenation request id
+        :return: The status of the request
+        """
+        s3 = boto3.resource("s3")
+        key = request_id + ".json"
+
+        try:
+            response = s3.Object(bucket_name=BUCKET_NAME, key=key).get()
+            body = json.loads(response['Body'].read())
+            return RequestStatus(body["status"])
+
+        except ClientError as e:
+            if e.response['Error']['Code'] == "NoSuchKey":
+                return RequestStatus("UNINITIALIZED")
+            else:
+                raise e
