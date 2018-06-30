e832b048f6e38194a73ccd7c5eef570c985d654c
Liu Han <hanliu061433@gmail.com>
2018-06-29 17:41:14 -0700

    A script to upload a directory of a directory of files into dss as bundles

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger = logging.getLogger("hca-matrix-service-testcase")
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
