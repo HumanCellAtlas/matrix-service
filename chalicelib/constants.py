32111caf4880c236f61b22e8b4ee663c4a4108ba
Liu Han <hanliu061433@gmail.com>
2018-06-29 17:41:14 -0700

    A script to upload a directory of a directory of files into dss as bundles

--- chalicelib/constants.py
+++ chalicelib/constants.py
@@ -1,2 +1,14 @@
+import os
+
+# S3 Bucket for storing merged matrices
 MERGED_MTX_BUCKET_NAME = "hca-dcp-matrix-service"
+
+# S3 Bucket for storing matrices concatenation request status
 MERGED_REQUEST_STATUS_BUCKET_NAME = "hca-dcp-matrix-service-request-status"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_DIR = os.path.join(BASE_DIR, 'config')
+REQUEST_TEMPLATE_PATH = os.path.join(CONFIG_DIR, 'request.json')
+BUNDLE_UUIDS_PATH = os.path.join(CONFIG_DIR, "bundle_uuids.json")
+
+JSON_EXTENSION = ".json"
---
+++ tests/upload_sample_matrices.py
@@ -0,0 +1,39 @@
+import json
+import os
+import sys
+import hca
+
+from chalicelib.constants import BUNDLE_UUIDS_PATH
+from tests import logger
+
+STAGING_BUCKET = "matrix-service-test"
+
+
+def upload_sample_matrices(dir_path):
+    """
+    Upload a bunch of matrices from a local directory to dss
+    :param dir_path: Directory that contains directories of matrices
+    """
+    bundle_uuids = []
+
+    for path in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, path)
+
+        if not os.path.isdir(file_path):
+            continue
+
+        logger.info("Uploading {} to DSS.".format(path))
+
+        client = hca.dss.DSSClient()
+        response = client.upload(src_dir=file_path, replica="aws", staging_bucket=STAGING_BUCKET)
+        bundle_uuids.append(response["bundle_uuid"])
+
+    # Store uuids for each uploaded directory into a bundle
+    with open(BUNDLE_UUIDS_PATH, "w") as f:
+        json.dump(bundle_uuids, f)
+
+    logger.info("Done.")
+
+
+if __name__ == "__main__":
+    upload_sample_matrices(sys.argv[1])
