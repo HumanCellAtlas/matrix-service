d0172e75c318a53986760797150ea57247a74a98
Liu Han <hanliu061433@gmail.com>
2018-07-05 14:43:19 -0700

    Modify test code for generate_request_id() function

--- test/request_handler_tests.py
+++ test/request_handler_tests.py
@@ -7,16 +7,27 @@ from chalicelib.request_handler import RequestHandler
 
 class TestRequestHandler(unittest.TestCase):
     def test_generate_request_id(self):
+        """
+        Request ids generated on uuids with different order should always
+        be same.
+        """
         bundle_uuids = []
 
         # Generate 10 random uuids
         for _ in range(10):
             bundle_uuids.append(rand_uuid())
 
+        bundle_uuids_copy = bundle_uuids.copy()
+        shuffle(bundle_uuids_copy)
+
+        # Make sure two list are in different order
+        while bundle_uuids_copy == bundle_uuids:
+            shuffle(bundle_uuids_copy)
+
         # Request ids generated for uuids in different order should be same
         self.assertEqual(
-            RequestHandler.generate_request_id(shuffle(bundle_uuids)),
-            RequestHandler.generate_request_id(shuffle(bundle_uuids))
+            RequestHandler.generate_request_id(bundle_uuids),
+            RequestHandler.generate_request_id(bundle_uuids_copy)
         )
 
     def test_check_request_status(self):
