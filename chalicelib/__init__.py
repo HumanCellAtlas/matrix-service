6e4f0792a32645d85a4ce538f537133e6652f280
Liu Han <hanliu061433@gmail.com>
2018-07-05 15:00:00 -0700

    Add a function for generating a list of random uuids

--- chalicelib/__init__.py
+++ chalicelib/__init__.py
@@ -12,3 +12,17 @@ logger.addHandler(stream_handler)
 
 def rand_uuid():
     return str(uuid.uuid4())
+
+
+def rand_uuids(n):
+    """
+    Generate a random list of uuids.
+    :param n: Number of uuid to generate.
+    :return: A list of random uuids.
+    """
+    uuids = []
+
+    for _ in range(n):
+        uuids.append(rand_uuid())
+
+    return uuids
