3b15776dcbf22e4c45a38d5371d7cf8f4466f85b
Liu Han <hanliu061433@gmail.com>
2018-07-05 16:30:14 -0700

    Write a function for generating a random number of directories

--- chalicelib/__init__.py
+++ chalicelib/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import random
+import tempfile
 import uuid
 
 formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
@@ -14,15 +16,37 @@ def rand_uuid():
     return str(uuid.uuid4())
 
 
-def rand_uuids(n):
+def rand_uuids():
     """
     Generate a random list of uuids.
-    :param n: Number of uuid to generate.
     :return: A list of random uuids.
     """
     uuids = []
+    n = random.randint(1, 11)
 
     for _ in range(n):
         uuids.append(rand_uuid())
 
     return uuids
+
+
+def mk_temp_dirs():
+    """
+    Generate a random list of temp directories containing some random files.
+    :return: A list of temp directories.
+    """
+    temp_dirs = []
+    n = random.randint(1, 11)
+    suffices = ('.json', '.loom', '.cvs', '',)
+
+    # Generate n directories
+    for _ in range(n):
+        temp_dir = tempfile.mkdtemp()
+        temp_dirs.append(temp_dir)
+        k = random.randint(1, 11)
+
+        # Generate k random files within each directory
+        for _ in range(k):
+            tempfile.mktemp(suffix=random.choice(suffices), dir=temp_dir)
+
+    return temp_dirs
