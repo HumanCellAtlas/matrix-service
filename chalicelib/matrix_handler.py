4a84c4743638302be8610c54e5ea4c11c0c9ac6e
Liu Han <hanliu061433@gmail.com>
2018-07-06 15:17:35 -0700

    Modify matrix_handler.py to make sure all created temp file will be removed after uses

--- chalicelib/matrix_handler.py
+++ chalicelib/matrix_handler.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 import tempfile
 import boto3
 import hca
@@ -7,7 +8,8 @@ import loompy
 
 from abc import ABC, abstractmethod
 from botocore.exceptions import ClientError
-from chalicelib.constants import MERGED_MTX_BUCKET_NAME, MERGED_REQUEST_STATUS_BUCKET_NAME, JSON_EXTENSION
+from chalicelib.constants import MERGED_MTX_BUCKET_NAME, \
+    MERGED_REQUEST_STATUS_BUCKET_NAME, JSON_EXTENSION
 from chalicelib.request_handler import RequestHandler, RequestStatus
 from chalicelib import logger
 
@@ -45,42 +47,45 @@ class MatrixHandler(ABC):
         TODO: Is there a way to directly connect to a s3 file instead of downloading it locally?
 
         :param bundle_uuids: A list of bundle uuids
-        :return: A list of downloaded local matrix files paths
+        :return: A list of downloaded local matrix files paths and their directory
         """
         logger.info("Downloading matrices from bundles: %s.", str(bundle_uuids))
 
         # Filter uuids of matrix files within each bundle
         mtx_uuids = self._filter_mtx(bundle_uuids)
 
+        # List to store downloaded mtx file paths
+        local_mtx_paths = []
+
         # Create a temp directory for storing s3 matrix files
         temp_dir = tempfile.mkdtemp()
 
-        local_mtx_paths = []
-
         # Download matrices from cloud to a local temp directory
         for uuid in mtx_uuids:
-            path = os.path.join(temp_dir, uuid + self._extension)
+            _, path = tempfile.mkstemp(suffix=self._extension, dir=temp_dir)
             with open(path, "wb") as mtx:
                 mtx.write(self.hca_client.get_file(uuid=uuid, replica="aws"))
             local_mtx_paths.append(path)
 
         logger.info("Done downloading %d matrix files.", len(local_mtx_paths))
-        return local_mtx_paths
+        return temp_dir, local_mtx_paths
 
     @abstractmethod
-    def _concat_mtx(self, paths, request_id):
+    def _concat_mtx(self, mtx_paths, mtx_dir, request_id):
         """
-        Concatenate a list of matrices, and save into a new file on disk
+        Concatenate a list of matrices, and save into a new file on disk.
 
-        :param paths: A list of downloaded local matrix files paths
-        :param request_id: The request id for the matrices concatenation
-        :return: New concatenated matrix file's path
+        :param mtx_paths: A list of downloaded local matrix files paths.
+        :param mtx_dir: The directory that contains the matrices.
+        :param request_id: The request id for the matrices concatenation.
+        :return: New concatenated matrix file's path.
         """
 
     def _upload_mtx(self, path):
         """
-        Upload a matrix file into an s3 bucket
-        :param path: Path of the merged matrix
+        Upload a matrix file into an s3 bucket.
+        :param path: Path of the merged matrix.
+        :return: S3 bucket key for uploading file.
         """
         logger.info("%s", "Uploading \"{}\" to s3 bucket: \"{}\".".format(os.path.basename(path), MERGED_MTX_BUCKET_NAME))
         s3 = boto3.resource("s3")
@@ -88,6 +93,11 @@ class MatrixHandler(ABC):
         with open(path, "rb") as merged_matrix:
             s3.Bucket(MERGED_MTX_BUCKET_NAME).put_object(Key=key, Body=merged_matrix)
         logger.info("Done uploading.")
+
+        # Remove local merged mtx after uploading it to s3
+        shutil.rmtree(os.path.dirname(path))
+
+        return key
 
     def run_merge_request(self, bundle_uuids, request_id):
         """
@@ -97,8 +107,8 @@ class MatrixHandler(ABC):
         :param request_id: Merge request id
         :return:
         """
-        mtx_paths = self._download_mtx(bundle_uuids)
-        merged_mtx_path = self._concat_mtx(mtx_paths, request_id)
+        mtx_dir, mtx_paths = self._download_mtx(bundle_uuids)
+        merged_mtx_path = self._concat_mtx(mtx_paths, mtx_dir, request_id)
         self._upload_mtx(merged_mtx_path)
 
         # Update the request status
@@ -131,11 +141,16 @@ class LoomMatrixHandler(MatrixHandler):
     """
     def __init__(self):
         super().__init__(".loom")
-        self._merged_mtx_dir = tempfile.mkdtemp()
 
-    def _concat_mtx(self, paths, request_id):
-        out_file = os.path.join(self._merged_mtx_dir, request_id + self._extension)
-        logger.info("Combining matrices to %s.", request_id + self._extension)
-        loompy.combine(paths, out_file)
-        logger.info("Done combining.")
+    def _concat_mtx(self, mtx_paths, mtx_dir, request_id):
+        try:
+            merged_mtx_dir = tempfile.mkdtemp()
+            out_file = os.path.join(merged_mtx_dir, request_id + self._extension)
+            logger.info("Combining matrices to %s.", out_file)
+            loompy.combine(mtx_paths, out_file)
+            logger.info("Done combining.")
+
+        finally:
+            shutil.rmtree(mtx_dir)
+
         return out_file
--- test/matrix_handler_tests.py
+++ test/matrix_handler_tests.py
@@ -5,50 +5,51 @@ import unittest
 import hca
 
 from chalicelib import mk_rand_dirs, scan_dirs, get_random_existing_bundle_uuids, \
-    check_s3key_existence
+    check_s3key_existence, mk_rand_loom_file, rand_uuid
 from chalicelib.constants import STAGING_BUCKET_NAME, MERGED_MTX_BUCKET_NAME
 from chalicelib.matrix_handler import LoomMatrixHandler
 
 
 class TestMatrixHandler(unittest.TestCase):
-    # def test_filter_mtx(self):
-    #     """
-    #     Make sure _filter_mtx() always filters correct number of mtx from
-    #     DSS bundles.
-    #     """
-    #
-    #     # Generate a random number of temp directories
-    #     temp_dirs = mk_temp_dirs()
-    #
-    #     # Get the number of ".loom" matrix files within the directories
-    #     local_mtx_num = scan_dirs(temp_dirs, ".loom")
-    #
-    #     client = hca.dss.DSSClient()
-    #     bundle_uuids = []
-    #
-    #     # Upload all temp directories into DSS as bundles
-    #     try:
-    #         for temp_dir in temp_dirs:
-    #             response = client.upload(
-    #                 src_dir=temp_dir,
-    #                 replica="aws",
-    #                 staging_bucket=STAGING_BUCKET_NAME
-    #             )
-    #             bundle_uuids.append(response["bundle_uuid"])
-    #
-    #         mtx_handler = LoomMatrixHandler()
-    #         remote_mtx_num = len(mtx_handler._filter_mtx(bundle_uuids))
-    #         self.assertEqual(local_mtx_num, remote_mtx_num)
-    #     finally:
-    #         for temp_dir in temp_dirs:
-    #             shutil.rmtree(temp_dir)
+    def test_filter_mtx(self):
+        """
+        Make sure _filter_mtx() always filters correct number of mtx from
+        DSS bundles.
+        """
+
+        # # Generate a random number of temp directories containing some
+        # # random files
+        # temp_dirs = mk_rand_dirs()
+        #
+        # # Get the number of ".loom" matrix files within the directories
+        # local_mtx_num = scan_dirs(temp_dirs, ".loom")
+        #
+        # client = hca.dss.DSSClient()
+        # bundle_uuids = []
+        #
+        # # Upload all temp directories into DSS as bundles
+        # try:
+        #     for temp_dir in temp_dirs:
+        #         response = client.upload(
+        #             src_dir=temp_dir,
+        #             replica="aws",
+        #             staging_bucket=STAGING_BUCKET_NAME
+        #         )
+        #         bundle_uuids.append(response["bundle_uuid"])
+        #
+        #     mtx_handler = LoomMatrixHandler()
+        #     remote_mtx_num = len(mtx_handler._filter_mtx(bundle_uuids))
+        #     self.assertEqual(local_mtx_num, remote_mtx_num)
+        # finally:
+        #     for temp_dir in temp_dirs:
+        #         shutil.rmtree(temp_dir)
 
     def test_download_mtx(self):
         """
         Make sure that mtx file paths returned from _download_mtx() exist.
         """
         # Get a random subset of bundle_uuids from sample bundle uuids
-        bundle_uuids_subset = get_random_existing_bundle_uuids()
+        bundle_uuids_subset = get_random_existing_bundle_uuids(ub=5)
 
         # Download mtx from bundles
         mtx_handler = LoomMatrixHandler()
@@ -66,12 +67,26 @@ class TestMatrixHandler(unittest.TestCase):
         Make sure that _upload_mtx() can successfully upload the merged matrix,
         and delete the local merged matrix file after uploading it.
         """
-        _, path = tempfile.mkstemp()
+        temp_dir = tempfile.mkdtemp()
+        _, path = tempfile.mkstemp(dir=temp_dir)
         mtx_handler = LoomMatrixHandler()
         key = mtx_handler._upload_mtx(path)
 
         self.assertFalse(os.path.exists(path))
         self.assertTrue(check_s3key_existence(key, MERGED_MTX_BUCKET_NAME))
+
+    def test_concat_mtx(self):
+        """
+        Make sure after concatenation, all downloaded matrices will be removed.
+        """
+        # mtx_dir, mtx_paths = mk_rand_loom_file(ub=5)
+        # mtx_handler = LoomMatrixHandler()
+        # mtx_handler._concat_mtx(mtx_paths, mtx_dir, rand_uuid())
+        #
+        # self.assertFalse(os.path.exists(mtx_dir))
+        #
+        # for mtx_path in mtx_paths:
+        #     self.assertFalse(os.path.exists(mtx_path))
 
 
 if __name__ == '__main__':
