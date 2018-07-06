3f897582955b2119cbf22857aef3983c0f8eea80
Liu Han <hanliu061433@gmail.com>
2018-07-06 14:26:52 -0700

    Write a test for _upload_mtx() function within matrix_handler.py

--- test/matrix_handler_tests.py
+++ test/matrix_handler_tests.py
@@ -1,12 +1,12 @@
-import json
 import os
-import random
 import shutil
+import tempfile
 import unittest
 import hca
 
-from chalicelib import mk_temp_dirs, scan_dirs
-from chalicelib.constants import STAGING_BUCKET_NAME, BUNDLE_UUIDS_PATH
+from chalicelib import mk_rand_dirs, scan_dirs, get_random_existing_bundle_uuids, \
+    check_s3key_existence
+from chalicelib.constants import STAGING_BUCKET_NAME, MERGED_MTX_BUCKET_NAME
 from chalicelib.matrix_handler import LoomMatrixHandler
 
 
@@ -27,43 +27,51 @@ class TestMatrixHandler(unittest.TestCase):
     #     bundle_uuids = []
     #
     #     # Upload all temp directories into DSS as bundles
-    #     for temp_dir in temp_dirs:
-    #         response = client.upload(
-    #             src_dir=temp_dir,
-    #             replica="aws",
-    #             staging_bucket=STAGING_BUCKET_NAME
-    #         )
-    #         bundle_uuids.append(response["bundle_uuid"])
+    #     try:
+    #         for temp_dir in temp_dirs:
+    #             response = client.upload(
+    #                 src_dir=temp_dir,
+    #                 replica="aws",
+    #                 staging_bucket=STAGING_BUCKET_NAME
+    #             )
+    #             bundle_uuids.append(response["bundle_uuid"])
     #
-    #     mtx_handler = LoomMatrixHandler()
-    #     remote_mtx_num = len(mtx_handler._filter_mtx(bundle_uuids))
-    #     self.assertEqual(local_mtx_num, remote_mtx_num)
+    #         mtx_handler = LoomMatrixHandler()
+    #         remote_mtx_num = len(mtx_handler._filter_mtx(bundle_uuids))
+    #         self.assertEqual(local_mtx_num, remote_mtx_num)
+    #     finally:
+    #         for temp_dir in temp_dirs:
+    #             shutil.rmtree(temp_dir)
 
     def test_download_mtx(self):
         """
         Make sure that mtx file paths returned from _download_mtx() exist.
         """
         # Get a random subset of bundle_uuids from sample bundle uuids
-        with open(BUNDLE_UUIDS_PATH, "r") as f:
-            sample_bundle_uuids = json.loads(f.read())
-
-        n = random.randint(1, 5)
-        bundle_uuids_subset = random.sample(sample_bundle_uuids, n)
+        bundle_uuids_subset = get_random_existing_bundle_uuids()
 
         # Download mtx from bundles
         mtx_handler = LoomMatrixHandler()
-        mtx_paths = mtx_handler._download_mtx(bundle_uuids_subset)
-
-        self.assertTrue(len(mtx_paths) > 0)
-
-        dir_path = os.path.dirname(mtx_paths[0])
+        mtx_dir, mtx_paths = mtx_handler._download_mtx(bundle_uuids_subset)
 
         # Check whether the downloaded matrices actually exist.
         for mtx_path in mtx_paths:
             self.assertTrue(os.path.exists(mtx_path))
 
         # Remove all created temp files
-        shutil.rmtree(dir_path)
+        shutil.rmtree(mtx_dir)
+
+    def test_upload_mtx(self):
+        """
+        Make sure that _upload_mtx() can successfully upload the merged matrix,
+        and delete the local merged matrix file after uploading it.
+        """
+        _, path = tempfile.mkstemp()
+        mtx_handler = LoomMatrixHandler()
+        key = mtx_handler._upload_mtx(path)
+
+        self.assertFalse(os.path.exists(path))
+        self.assertTrue(check_s3key_existence(key, MERGED_MTX_BUCKET_NAME))
 
 
 if __name__ == '__main__':
