24f2004a4f4dbe3c18904817f5a385eca0d7eed1
Liu Han <hanliu061433@gmail.com>
2018-07-09 14:30:16 -0700

    Write a test for run_merge_request() under matrix_handler module

--- chalicelib/constants.py
+++ chalicelib/constants.py
@@ -24,7 +24,7 @@ REQUEST_TEMPLATE = {
     "merged_mtx_url": ""
 }
 
-# Paths configuration
+# Path configuration
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEST_DIR = os.path.join(BASE_DIR, 'test')
 BUNDLE_UUIDS_PATH = os.path.join(TEST_DIR, "bundle_uuids.json")
--- test/matrix_handler_tests.py
+++ test/matrix_handler_tests.py
@@ -2,8 +2,9 @@ import shutil
 import unittest
 
 from chalicelib import *
-from chalicelib.constants import MERGED_MTX_BUCKET_NAME
+from chalicelib.constants import MERGED_MTX_BUCKET_NAME, REQUEST_STATUS_BUCKET_NAME
 from chalicelib.matrix_handler import LoomMatrixHandler
+from chalicelib.request_handler import RequestHandler
 
 
 class TestMatrixHandler(unittest.TestCase):
@@ -38,7 +39,34 @@ class TestMatrixHandler(unittest.TestCase):
         self.assertFalse(os.path.exists(path))
         self.assertTrue(s3key_exists(key, MERGED_MTX_BUCKET_NAME))
 
+        # Delete the key after checking
         delete_s3key(key=key, bucket_name=MERGED_MTX_BUCKET_NAME)
+
+    def test_run_merge_request(self):
+        """
+        Make sure that run_merge_request() can successfully create a merged matrix and
+        a request status file in s3. (Assumption: Matrices concatenation always works
+        correctly.)
+        """
+        # Get a random subset of bundle_uuids from sample bundle uuids
+        bundle_uuids_subset = get_random_existing_bundle_uuids(ub=5)
+
+        # Generate a request id based on bundle uuids
+        request_id = RequestHandler.generate_request_id(bundle_uuids_subset)
+
+        # Run merge request
+        mtx_handler = LoomMatrixHandler()
+        mtx_handler.run_merge_request(bundle_uuids=bundle_uuids_subset, request_id=request_id)
+
+        merged_mtx_key = request_id + ".loom"
+        request_status_key = request_id + ".json"
+
+        self.assertTrue(s3key_exists(key=merged_mtx_key, bucket_name=MERGED_MTX_BUCKET_NAME))
+        self.assertTrue(s3key_exists(key=request_status_key, bucket_name=REQUEST_STATUS_BUCKET_NAME))
+
+        # Delete keys after checking
+        delete_s3key(key=merged_mtx_key, bucket_name=MERGED_MTX_BUCKET_NAME)
+        delete_s3key(key=request_status_key, bucket_name=REQUEST_STATUS_BUCKET_NAME)
 
 
 if __name__ == '__main__':
