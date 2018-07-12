98a7515c6d7048ce82af9ef14fd6ae851847930b
Liu Han <hanliu061433@gmail.com>
2018-07-12 14:25:20 -0700

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "hca"
+  region = "${var.aws_region}"
+  profile = "${var.aws_profile}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_profile" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
