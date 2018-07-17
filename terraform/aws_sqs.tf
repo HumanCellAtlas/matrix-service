a8b45056654450adeed6e5ee2596c977e97f9cc6
Liu Han <hanliu061433@gmail.com>
2018-07-17 12:47:11 -0700

    Set SQS visibility timeout to be 300

--- terraform/aws_sqs.tf
+++ terraform/aws_sqs.tf
@@ -9,7 +9,7 @@ data "template_file" "aws_sqs_queue_policy" {
 
 resource "aws_sqs_queue" "sqs" {
   name                       = "${var.ms-sqs-queue}"
-  visibility_timeout_seconds = 60
+  visibility_timeout_seconds = 300
 }
 
 resource "aws_sqs_queue_policy" "sqs_policy" {
