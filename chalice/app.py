89dd0b6b422103ba203c76f4662efd801e55f802
Liu Han <hanliu061433@gmail.com>
2018-07-20 15:18:31 -0700

    Implement an endpoint for checking request status

--- chalice/app.py
+++ chalice/app.py
@@ -12,6 +12,30 @@ from chalicelib.sqs import SqsQueueHandler
 app = Chalice(app_name='matrix-service-api')
 app.debug = True
 
+# Replace handler here for supporting concatenation on other matrix formats
+mtx_handler = LoomMatrixHandler()
+
+
 @app.route('/matrices/health', methods=['GET'])
 def health():
     return {'status': 'OK'}
+
+
+@app.route('/matrices/concat/{request_id}', methods=['GET'])
+def check_request_status(request_id):
+    """
+    Check the status of a matrices concatenation request based on
+    a request ID.
+
+    :param request_id: Matrices concatenation request ID.
+    """
+    try:
+        merge_request = RequestHandler.get_request(request_id=request_id)
+        merge_request_body = json.loads(merge_request)
+        return merge_request_body
+
+    except BlobNotFoundError:
+        raise NotFoundError("Request({}) has not been initialized.".format(request_id))
+
+    except BlobStoreUnknownError:
+        raise BadRequestError(traceback.format_exc())
