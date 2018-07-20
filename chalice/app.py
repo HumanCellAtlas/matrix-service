eb2312578ae9f200c65a4fb07d1050ac351409f0
Liu Han <hanliu061433@gmail.com>
2018-07-20 16:05:15 -0700

    Implement the lambda function listening on SQS queue for launching asyn matrices-concatenation requests

--- chalice/app.py
+++ chalice/app.py
@@ -73,3 +73,51 @@ def concat_matrices():
         raise BadRequestError(error_msg)
 
     return {"request_id": request_id}
+
+
+def ms_sqs_queue_listener(event, context):
+    """
+    Create a lambda function that listens for the matrix service's SQS
+    queue events.Once it detects an incoming message on the queue, it
+    will process it by launching a job to do the matrices concatenation
+    based on the message content.
+    :param event: SQS Queue event.
+    """
+    for record in event["Records"]:
+        record_body = json.loads(record["body"])
+        bundle_uuids = record_body["bundle_uuids"]
+        request_id = RequestHandler.generate_request_id(bundle_uuids)
+
+        """
+        Check whether job id has a corresponding match in s3:
+        If yes, then proceed the matrices concatenation.
+        If no, it means either of the following cases happens:
+          1. Message sent to SQS Queue has been corrupted; As a result,
+             the job id sent by API Gateway + Lambda is different from
+             the one received by the SQS queue + Lambda.
+          2. Before receiving the msg from SQS queue, a duplicate mtx
+             concatenation request has been initialized. As a result,
+             the job id in the status file in s3 has been updated to
+             a new value.
+        Therefore, we need to stall the matrices concatenation to avoid
+        either doing duplicate concatenation or concatenation on incorrect
+        set of matrices.
+        """
+        try:
+            merge_request = RequestHandler.get_request(request_id=request_id)
+            merge_request_body = json.loads(merge_request)
+            job_id = merge_request_body["job_id"]
+
+            # Stall concatenation process if any of these cases described above happens
+            if not job_id or job_id != record_body["job_id"]:
+                return
+
+            mtx_handler.run_merge_request(
+                bundle_uuids=bundle_uuids,
+                request_id=request_id,
+                job_id=record_body["job_id"]
+            )
+
+        except (BlobStoreUnknownError, SwaggerAPIException, Exception):
+            error_msg = traceback.format_exc()
+            logger.exception(error_msg)
