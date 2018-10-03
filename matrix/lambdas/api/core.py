252f525ae021fbf94e28d5dd9723a6ddd047c24a
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-03 13:22:51 -0700

    add post_matrix parameter usage validation

--- matrix/lambdas/api/core.py
+++ matrix/lambdas/api/core.py
@@ -4,11 +4,35 @@ import requests
 
 import boto3
 
+from ...common.constants import MatrixFormat
+
 LAMBDA_CLIENT = boto3.client("lambda",
                              region_name=os.environ['AWS_DEFAULT_REGION'])
 
 
 def post_matrix(body):
+    has_ids = 'bundle_fqids' in body
+    has_url = 'bundle_fqids_url' in body
+
+    format = body['format'] if 'format' in body else MatrixFormat.ZARR.value
+
+    # validate input parameters
+    if not (has_ids or has_url):
+        return {
+                   'code': requests.codes.bad_request,
+                   'message': "Missing required parameter. "
+                              "One of `bundle_fqids` or `bundle_fqids_url` must be supplied. "
+                              "Visit https://matrix.dev.data.humancellatlas.org for more information."
+               }, requests.codes.bad_request
+
+    if has_ids and has_url:
+        return {
+                   'code': requests.codes.bad_request,
+                   'message': "Invalid parameters supplied. "
+                              "Must supply either one of `bundle_fqids` or `bundle_fqids_url`. "
+                              "Visit https://matrix.dev.data.humancellatlas.org for more information."
+               }, requests.codes.bad_request
+
     LAMBDA_CLIENT.invoke(
         FunctionName=os.environ['DRIVER_FN_NAME'],
         InvocationType="Event",
