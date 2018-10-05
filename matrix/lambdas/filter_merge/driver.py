7401c6f5db07ef3c8f916a111f2729af5a26a393
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-04 22:31:25 -0700

    implement Driver task to handle bundle ids

--- matrix/lambdas/filter_merge/driver.py
+++ matrix/lambdas/filter_merge/driver.py
@@ -1,2 +1,33 @@
-def driver():
-    pass
+import typing
+
+from ...common.dynamo_handler import DynamoHandler
+from ...common.lambda_handler import LambdaHandler
+from ...common.lambda_handler import LambdaName
+
+
+class Driver:
+    """
+    The first task in a distributed filter merge job.
+    """
+    def __init__(self):
+        self.lambda_handler = LambdaHandler()
+        self.dynamo_handler = DynamoHandler()
+
+    def run(self, request_id: str, bundle_fqids: typing.List[str], format: str):
+        """
+        Initialize a filter merge job and spawn a mapper task for each bundle_fqid.
+
+        :param request_id: Filter merge job request ID
+        :param bundle_fqids: List of bundle fqids to be queried on
+        :param format: MatrixFormat file format of expression matrices
+        """
+        self.dynamo_handler.init_state_table(request_id, len(bundle_fqids))
+
+        for fqid in bundle_fqids:
+            mapper_payload = {
+                'request_id': request_id,
+                'bundle_fqid': fqid,
+                'format': format,
+            }
+
+            self.lambda_handler.invoke(LambdaName.MAPPER, mapper_payload)
--- tests/unit/lambdas/filter_merge/test_driver.py
+++ tests/unit/lambdas/filter_merge/test_driver.py
@@ -1,7 +1,29 @@
 import unittest
+from unittest import mock
+import uuid
+
+from matrix.common.lambda_handler import LambdaName
+from matrix.lambdas.filter_merge.driver import Driver
 
 
 class TestDriver(unittest.TestCase):
+    def setUp(self):
+        self._driver = Driver()
 
-    def test_something(self):
-        pass
+    @mock.patch("matrix.common.dynamo_handler.DynamoHandler.init_state_table")
+    @mock.patch("matrix.common.lambda_handler.LambdaHandler.invoke")
+    def test_run(self, mock_lambda_invoke, mock_dynamo_init_state_table):
+        request_id = str(uuid.uuid4())
+        bundle_fqids = ["id1", "id2"]
+        format = "test_format"
+
+        self._driver.run(request_id, bundle_fqids, format)
+
+        mock_dynamo_init_state_table.assert_called_once_with(request_id, len(bundle_fqids))
+        mock_lambda_invoke.assert_called_with(LambdaName.MAPPER,
+                                              {
+                                                  'request_id': request_id,
+                                                  'bundle_fqid': bundle_fqids[1],
+                                                  'format': format,
+                                              })
+        self.assertEqual(mock_lambda_invoke.call_count, len(bundle_fqids))
