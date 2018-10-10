8c7ef23ac5bfb0654c538d9dfe8baec166e35d82
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-10 09:10:52 -0700

    fix mapper lambda env vars

--- terraform/modules/matrix-service/lambdas/mapper_lambda.tf
+++ terraform/modules/matrix-service/lambdas/mapper_lambda.tf
@@ -73,6 +73,7 @@ resource "aws_lambda_function" "matrix_service_mapper_lambda" {
       DEPLOYMENT_STAGE = "${var.deployment_stage}"
       LAMBDA_WORKER_FUNCTION_NAME="dcp-matrix-service-worker-${var.deployment_stage}"
       DYNAMO_STATE_TABLE_NAME="dcp-matrix-service-state-table-${var.deployment_stage}"
+      DYNAMO_OUTPUT_TABLE_NAME="dcp-matrix-service-output-table-${var.deployment_stage}"
     }
   }
 }
