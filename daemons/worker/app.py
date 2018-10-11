a074245c39b57c0d7369f0381644315fd8a97f2a
parthshahva <parthshahva@gmail.com>
2018-10-11 10:59:14 -0700

    change work_chunk_spec to worker_chunk_spec

--- daemons/worker/app.py
+++ daemons/worker/app.py
@@ -2,11 +2,11 @@ from matrix.lambdas.daemons.worker import Worker
 
 
 def worker_handler(event, context):
-    assert 'request_id' in event and 'work_chunk_spec' in event and 'format' in event and 'filter_string' in event
+    assert 'request_id' in event and 'worker_chunk_spec' in event and 'format' in event and 'filter_string' in event
 
-    work_chunk_spec = event['work_chunk_spec']
-    assert 'bundle_uuid' in work_chunk_spec and 'bundle_version' in work_chunk_spec
-    assert 'start_row' in work_chunk_spec and 'num_rows' in work_chunk_spec
+    worker_chunk_spec = event['worker_chunk_spec']
+    assert 'bundle_uuid' in worker_chunk_spec and 'bundle_version' in worker_chunk_spec
+    assert 'start_row' in worker_chunk_spec and 'num_rows' in worker_chunk_spec
 
     worker = Worker()
-    worker.run(event['request_id'], event['filter_string'], work_chunk_spec)
+    worker.run(event['request_id'], event['filter_string'], worker_chunk_spec)
--- matrix/lambdas/daemons/worker.py
+++ matrix/lambdas/daemons/worker.py
@@ -10,12 +10,12 @@ class Worker:
         self.lambda_handler = LambdaHandler()
         self.dynamo_handler = DynamoHandler()
 
-    def run(self, request_id: str, filter_string: str, work_chunk_spec: dict):
+    def run(self, request_id: str, filter_string: str, worker_chunk_spec: dict):
         """
         Filter one work chunk and invoke reducer lambda when last worker job is completed.
 
         :param request_id: Filter merge job request ID
         :param filter string: user provided filter string to filter out results
-        :work_chunk_spec: dict with keys 'bundle_uuid', 'bundle_version', 'start_row', 'num_rows'
+        :worker_chunk_spec: dict with keys 'bundle_uuid', 'bundle_version', 'start_row', 'num_rows'
         """
         pass
