b1ca9bd06d285a739d22ec2685dbe9febd10fc0f
parthshahva <parthshahva@gmail.com>
2018-10-17 22:02:18 -0700

    initial setup for batch converter

--- terraform/modules/matrix-service/infra/variables.tf
+++ terraform/modules/matrix-service/infra/variables.tf
@@ -1,9 +1,15 @@
 variable "deployment_stage" {
   type = "string"
 }
+
 variable "account_id" {
   type = "string"
 }
+
 variable "aws_region" {
+  type = "string"
+}
+
+variable "converter_cluster_ami_id" {
   type = "string"
 }
