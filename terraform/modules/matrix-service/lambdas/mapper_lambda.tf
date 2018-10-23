588792f7f130d76b25b8d0b4afd7ca43db1d581c
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-22 19:56:37 -0700

    increase mapper lambda timeout to 15min

--- terraform/modules/matrix-service/lambdas/mapper_lambda.tf
+++ terraform/modules/matrix-service/lambdas/mapper_lambda.tf
@@ -67,7 +67,7 @@ resource "aws_lambda_function" "matrix_service_mapper_lambda" {
   role             = "${aws_iam_role.matrix_service_mapper_lambda.arn}"
   handler          = "app.mapper_handler"
   runtime          = "python3.6"
-  timeout          = 300
+  timeout          = 900
 
   environment {
     variables = {
