d00e6d62221af2cc77294af5638921e2763a5e45
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-22 19:58:14 -0700

    increase dynamo lock table throughput to 150

--- terraform/modules/matrix-service/infra/dynamo.tf
+++ terraform/modules/matrix-service/infra/dynamo.tf
@@ -13,8 +13,8 @@ resource "aws_dynamodb_table" "state-table" {
 
 resource "aws_dynamodb_table" "lock-table" {
   name           = "dcp-matrix-service-lock-table-${var.deployment_stage}"
-  read_capacity  = 25
-  write_capacity = 25
+  read_capacity  = 150
+  write_capacity = 150
   hash_key       = "LockKey"
 
   attribute {
