2bf835150a2ebb0f3125debc94e1838e250b96c9
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-10-23 01:29:34 -0700

    add logging to driver

--- tests/unit/lambdas/daemons/test_driver.py
+++ tests/unit/lambdas/daemons/test_driver.py
@@ -10,24 +10,24 @@ from matrix.lambdas.daemons.driver import Driver
 
 class TestDriver(unittest.TestCase):
     def setUp(self):
+        self.request_id = str(uuid.uuid4())
         self._bundles_per_worker = 100
-        self._driver = Driver(self._bundles_per_worker)
+        self._driver = Driver(self.request_id, self._bundles_per_worker)
 
     @mock.patch("matrix.common.dynamo_handler.DynamoHandler.create_state_table_entry")
     @mock.patch("matrix.common.dynamo_handler.DynamoHandler.create_output_table_entry")
     @mock.patch("matrix.common.lambda_handler.LambdaHandler.invoke")
     def test_run(self, mock_lambda_invoke, mock_dynamo_create_output_table_entry, mock_dynamo_create_state_table_entry):
-        request_id = str(uuid.uuid4())
         bundle_fqids = ["id1.version", "id2.version"]
         format = "test_format"
 
-        self._driver.run(request_id, bundle_fqids, format)
+        self._driver.run(bundle_fqids, format)
 
         num_mappers = math.ceil(len(bundle_fqids) / self._bundles_per_worker)
-        mock_dynamo_create_state_table_entry.assert_called_once_with(request_id, num_mappers)
-        mock_dynamo_create_output_table_entry.assert_called_once_with(request_id)
+        mock_dynamo_create_state_table_entry.assert_called_once_with(self.request_id, num_mappers)
+        mock_dynamo_create_output_table_entry.assert_called_once_with(self.request_id)
         expected_calls = [
-            call(LambdaName.MAPPER, {'request_id': request_id,
+            call(LambdaName.MAPPER, {'request_id': self.request_id,
                                      'bundle_fqids': bundle_fqids,
                                      'format': format})
         ]
