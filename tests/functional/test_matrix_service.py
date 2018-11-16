a0c010a6c0ca221f193546dc1d988b123b63f78f
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-11-16 15:20:47 -0800

    fix CSV output format

--- tests/functional/test_matrix_service.py
+++ tests/functional/test_matrix_service.py
@@ -89,10 +89,17 @@ class TestMatrixService(unittest.TestCase):
     def test_loom_output_matrix_service(self):
         self.request_id = self._post_matrix_service_request(
             bundle_fqids=INPUT_BUNDLE_IDS[self.dss_env], format="loom")
-        # timeout seconds is increased to 600 as batch may tak time to spin up spot instances for conversion.
+        # timeout seconds is increased to 1200 as batch may take time to spin up spot instances for conversion.
         WaitFor(self._poll_get_matrix_service_request, self.request_id)\
             .to_return_value(MatrixRequestStatus.COMPLETE.value, timeout_seconds=1200)
         self._analyze_loom_matrix_results(self.request_id, INPUT_BUNDLE_IDS[self.dss_env])
+
+    def test_csv_output_matrix_service(self):
+        self.request_id = self._post_matrix_service_request(
+            bundle_fqids=INPUT_BUNDLE_IDS[self.dss_env], format="csv")
+        # timeout seconds is increased to 1200 as batch may take time to spin up spot instances for conversion.
+        WaitFor(self._poll_get_matrix_service_request, self.request_id) \
+            .to_return_value(MatrixRequestStatus.COMPLETE.value, timeout_seconds=1200)
 
     def test_matrix_service_without_specified_output(self):
         self.request_id = self._post_matrix_service_request(
