b5802e732d0d7223cc9a1f9fac578f0a2117b3fe
Parth Shah <pshah@chanzuckerberg.com>
2019-02-22 14:45:43 -0800

    initial setup of query runner docker image and ecs/fargate terraform setup (#199)
    * initial setup of query runner docker image and ecs/fargate terraform setup
    
    * use correct image version
    
    * sqs and delete ununsed lambdas

---
+++ matrix/docker/query_runner.py
@@ -0,0 +1,10 @@
+"""Script to pull from sqs and run redshift queries. Will be dockerized."""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    print("STARTED query runner")
+    main()
---
+++ terraform/modules/matrix-service/infra/sqs.tf
@@ -0,0 +1,13 @@
+resource "aws_sqs_queue" "query_queue" {
+  name                      = "dcp-matrix-query-queue-${var.deployment_stage}"
+//  Queue visibility timeout must be larger than (triggered lambda) function timeout
+  visibility_timeout_seconds = 960
+  message_retention_seconds = 86400
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.query_deadletter_queue.arn}\",\"maxReceiveCount\":4}"
+
+}
+
+resource "aws_sqs_queue" "query_deadletter_queue" {
+  name                      = "dcp-matrix-query-deadletter-queue-${var.deployment_stage}"
+  message_retention_seconds = 1209600
+}
