27c9a0ae938d3db5f32a071b89e60caeb4ebd2e3
parthshahva <parthshahva@gmail.com>
2019-02-25 16:55:36 -0800

    change arn to url for sqs

--- terraform/modules/matrix-service/infra/secrets.tf
+++ terraform/modules/matrix-service/infra/secrets.tf
@@ -6,7 +6,7 @@ resource "aws_secretsmanager_secret_version" "infra_secrets" {
   secret_id = "${aws_secretsmanager_secret.infra_secrets.id}"
   secret_string = <<SECRETS_JSON
 {
-  "query_job_q_arn": "${aws_sqs_queue.query_queue.id}",
+  "query_job_q_url": "${aws_sqs_queue.query_queue.id}",
 }
 SECRETS_JSON
 }
