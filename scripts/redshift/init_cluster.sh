25d8061d1b9dd237260eed40f25dab6ed1b0db8c
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-03-01 11:16:43 -0800

    update loader bash script

--- scripts/redshift/init_cluster.sh
+++ scripts/redshift/init_cluster.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 instance_name="$(whoami)-$(date +'%Y-%m-%d-%H-%M')"
-aegea launch $instance_name --instance-type i3.large --ami aegea-base --wait-for-ssh
-aegea scp -- -r . ubuntu@$instance_name:~/
-aegea ssh ubuntu@$instance_name "sudo export DEPLOYMENT_STAGE=dev && sudo yum install python36 && pip install -r requirements.txt && python3 ~/init_cluster.py"
-aegea terminate $instance_name
+aegea launch $instance_name --instance-type i3.2xlarge --ami aegea-base5 --wait-for-ssh
+aegea ssh ubuntu@$instance_name "sudo chmod 777 /mnt && sudo mkfs -t ext4 /dev/nvme0n1 && sudo mount -t ext4 /dev/nvme0n1 /mnt && df -Th /mnt"
+aegea scp -- -r ./* ubuntu@$instance_name:/mnt
+aegea ssh ubuntu@$instance_name "cd /mnt && export DEPLOYMENT_STAGE=dev && sudo pip3 install -r requirements.txt && python3 init_cluster.py"
