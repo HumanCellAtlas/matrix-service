05cc10f6e3e8f12cc07d12fdd419eb783290aeac
parthshahva <parthshahva@gmail.com>
2019-03-22 14:02:07 -0700

    remove expiration policy on s3 results bucket

--- terraform/modules/matrix-service/infra/s3.tf
+++ terraform/modules/matrix-service/infra/s3.tf
@@ -1,17 +1,6 @@
 resource "aws_s3_bucket" "matrix-results" {
   bucket = "dcp-matrix-service-results-${var.deployment_stage}"
   acl    = "public-read"
-
-  lifecycle_rule {
-    id      = "matrix_service_results_expiration"
-
-    expiration {
-      days = 28
-    }
-
-    enabled = true
-  }
-  # Add tags later
 }
 
 resource "aws_s3_bucket_policy" "matrix_results_bucket_policy" {
