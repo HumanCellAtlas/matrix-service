0cd7bdf21da7a75ac63378dc3fbe02904dd7d368
parthshahva <parthshahva@gmail.com>
2019-05-13 16:07:17 -0700

    use transactions and log error if can't add/drop readonly user

--- scripts/redshift/setup_readonly_user.py
+++ scripts/redshift/setup_readonly_user.py
@@ -15,11 +15,14 @@ def handler():
     readonly_password = matrix_config.readonly_password
     drop_user_query = f"DROP USER IF EXISTS {readonly_username};"
     add_user_query = f"CREATE USER {readonly_username} WITH PASSWORD '{readonly_password}';"
-    grant_query = f"grant select on all tables in schema public to {readonly_username};"
-    redshift_handler.transaction([drop_user_query])
-    redshift_handler.transaction([add_user_query])
-    redshift_handler.transaction([grant_query])
-    
+    grant_public_query = f"grant select on all tables in schema public to {readonly_username};"
+    grant_information_schema_query = f"grant select on all tables in schema information_schema to {readonly_username};"
+    try:
+        redshift_handler.transaction([drop_user_query, add_user_query])
+    except Exception as e:
+        print(e)
+    redshift_handler.transaction([grant_public_query, grant_information_schema_query])
+    print("permissions applied to readonly user")
 
 if __name__ == '__main__':
     handler()
