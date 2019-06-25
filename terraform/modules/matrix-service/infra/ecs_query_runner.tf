b25b9857e6e8091aa4a1cf5fe41483743cf03605
Parth Shah <pshah@chanzuckerberg.com>
2019-06-24 20:52:05 -0700

    append request id to working directory for conversions (#318)
    * append request id to working dir for conversions
    
    * update query runner version

--- terraform/modules/matrix-service/infra/ecs_query_runner.tf
+++ terraform/modules/matrix-service/infra/ecs_query_runner.tf
@@ -45,7 +45,7 @@ resource "aws_ecs_task_definition" "query_runner" {
     ],
     "memory": 512,
     "cpu": 256,
-    "image": "humancellatlas/matrix-query-runner:10",
+    "image": "humancellatlas/matrix-query-runner:12",
     "name": "query-runner-${var.deployment_stage}",
     "logConfiguration": {
         "logDriver": "awslogs",
