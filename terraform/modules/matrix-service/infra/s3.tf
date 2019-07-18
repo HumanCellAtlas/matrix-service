fe550a5d0afbc4140e0fa440c408161a997ed796
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-07-18 13:34:12 -0700

    add 30 day expiration policy to results bucket

--- terraform/modules/matrix-service/infra/s3.tf
+++ terraform/modules/matrix-service/infra/s3.tf
@@ -1,6 +1,16 @@
 resource "aws_s3_bucket" "matrix-results" {
   bucket = "dcp-matrix-service-results-${var.deployment_stage}"
   acl    = "public-read"
+
+  lifecycle_rule {
+    id      = "matrix_service_results_expiration"
+
+    expiration {
+      days = 30
+    }
+
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket_policy" "matrix_results_bucket_policy" {
