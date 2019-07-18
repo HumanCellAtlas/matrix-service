c46f57dc8674ef8ab4b3be4c9d4e414273b670c9
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-07-18 12:50:26 -0700

    create service info Dynamo table

--- terraform/modules/matrix-service/infra/dynamo.tf
+++ terraform/modules/matrix-service/infra/dynamo.tf
@@ -10,3 +10,21 @@ resource "aws_dynamodb_table" "request_table" {
   }
   # Add tags to this resource
 }
+
+resource "aws_dynamodb_table" "data_table" {
+  name           = "dcp-matrix-service-data-table-${var.deployment_stage}"
+  read_capacity  = 25
+  write_capacity = 25
+  hash_key       = "Version"
+  range_key      = "Date"
+
+  attribute {
+    name = "Deployment"
+    type = "S"
+  }
+
+  attribute {
+    name = "Date"
+    type = "S"
+  }
+}
