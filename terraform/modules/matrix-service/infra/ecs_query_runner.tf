06f7f20345b1ed8edbb74d635a543f16d3f8b446
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2019-07-30 11:48:57 -0700

    Bump query runner version

--- terraform/modules/matrix-service/infra/ecs_query_runner.tf
+++ terraform/modules/matrix-service/infra/ecs_query_runner.tf
@@ -45,7 +45,7 @@ resource "aws_ecs_task_definition" "query_runner" {
     ],
     "memory": 512,
     "cpu": 256,
-    "image": "humancellatlas/matrix-query-runner:13",
+    "image": "humancellatlas/matrix-query-runner:14",
     "name": "query-runner-${var.deployment_stage}",
     "logConfiguration": {
         "logDriver": "awslogs",
