6a84d1e8a10224beccc430806b9faa2e435d0dcc
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-08-15 16:58:18 -0700

    fix unit tests

--- tests/unit/common/aws/test_dynamo_handler.py
+++ tests/unit/common/aws/test_dynamo_handler.py
@@ -49,6 +49,8 @@ class TestDynamoHandler(MatrixTestCaseUsingMockAWS):
 
         self.assertTrue(all(field.value in entry for field in RequestTableField))
         self.assertEqual(entry[RequestTableField.FORMAT.value], self.format)
+        self.assertEqual(entry[RequestTableField.DATA_VERSION.value], 0)
+        self.assertEqual(entry[RequestTableField.REQUEST_HASH.value], "N/A")
         self.assertEqual(entry[RequestTableField.EXPECTED_DRIVER_EXECUTIONS.value], 1)
         self.assertEqual(entry[RequestTableField.EXPECTED_CONVERTER_EXECUTIONS.value], 1)
         self.assertEqual(entry[RequestTableField.CREATION_DATE.value], stub_date)
