10c3d2476085c48b99ce72686906617388635b80
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-08-22 10:04:40 -0700

    create data version dynamo tf resources

--- terraform/modules/matrix-service/infra/dynamo.tf
+++ terraform/modules/matrix-service/infra/dynamo.tf
@@ -10,3 +10,27 @@ resource "aws_dynamodb_table" "request_table" {
   }
   # Add tags to this resource
 }
+
+resource "aws_dynamodb_table" "data_version_table" {
+  name           = "dcp-matrix-service-data-version-table-${var.deployment_stage}"
+  read_capacity  = 25
+  write_capacity = 25
+  hash_key       = "DataVersion"
+
+  attribute {
+    name = "DataVersion"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "deployment_table" {
+  name           = "dcp-matrix-service-deployment-table-${var.deployment_stage}"
+  read_capacity  = 25
+  write_capacity = 25
+  hash_key       = "Deployment"
+
+  attribute {
+    name = "Deployment"
+    type = "S"
+  }
+}
