547fc5ef80be387574020080625bfc5e426d7b59
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2019-08-27 11:52:32 -0700

    upgrade docker images

--- terraform/modules/matrix-service/infra/ecs_query_runner.tf
+++ terraform/modules/matrix-service/infra/ecs_query_runner.tf
@@ -53,7 +53,7 @@ resource "aws_ecs_task_definition" "query_runner" {
     ],
     "memory": 512,
     "cpu": 256,
-    "image": "humancellatlas/matrix-query-runner:18",
+    "image": "humancellatlas/matrix-query-runner:17",
     "name": "query-runner-${var.deployment_stage}",
     "logConfiguration": {
         "logDriver": "awslogs",
